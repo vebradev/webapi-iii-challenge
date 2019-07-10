4fe6e1535fe2d3cc57ec1b63a50f7ab599652a7e
vebradev <justinas@vebra.dev>
2019-07-10 15:27:59 +0300

    Add logger middleware.

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-const express = require('express');
+const express = require("express");
 const server = express();
-const userRouter = require('./users/userRouter');
+const userRouter = require("./users/userRouter");
 
 server.use(express.json());
+server.use(logger);
 server.use(userRouter);
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+  const log = {
+    method: req.method,
+    url: req.url,
+    time: new Date()
+  };
+  console.log(log);
+  next();
+}
 
 module.exports = server;
