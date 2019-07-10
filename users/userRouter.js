d516180279cd224f6dcb24186526eb2546dc076f
vebradev <justinas@vebra.dev>
2019-07-10 15:31:13 +0300

    Add few GET endpoints & validateUserID middleware.

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,58 @@
 const express = require("express");
-
 const router = express.Router();
+const User = require("./userDb");
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {});
 
+router.post("/:id/posts", (req, res) => {});
+
+router.get("/", (req, res) => {
+  User.get()
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Can't get users."
+      });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.get("/:id", validateUserId, (req, res) => {
+  User.getById(req.params.id)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: `Can't retrieve user by ID: ${req.params.id}.`
+      });
+    });
 });
 
-router.get('/some', (req, res) => {
-    res.send(`<h2>Some</h2>`)
-});
+router.get("/:id/posts", (req, res) => {});
 
-router.get('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {});
 
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
+  const user = await User.getById(req.params.id);
 
-};
+  if (user) {
+    req.user = user;
+    next();
+  } else {
+    res.status(400).json({
+      error: `No such user with ID ${req.params.id}.`
+    });
+  }
+}
 
-function validateUser(req, res, next) {
+function validateUser(req, res, next) {}
 
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
